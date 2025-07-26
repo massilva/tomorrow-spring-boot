a2c14539d52ad9a10694109f193245db96f1e639
Mvlm-lp <mvinnylopes01@gmail.com>
2025-07-26 15:22:12 -0300

    Add files via upload

---
+++ Controle-gastos-springboot/src/main/java/Controle_gastos/Controle_gastos_springboot/ControleGastosSpringbootApplication.java
@@ -0,0 +1,13 @@
+package Controle_gastos.Controle_gastos_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleGastosSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleGastosSpringbootApplication.class, args);
+	}
+
+}
---
+++ Controle-gastos-springboot/src/test/java/Controle_gastos/Controle_gastos_springboot/ControleGastosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package Controle_gastos.Controle_gastos_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleGastosSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
