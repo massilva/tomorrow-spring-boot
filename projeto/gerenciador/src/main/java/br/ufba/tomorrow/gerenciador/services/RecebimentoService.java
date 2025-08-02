92ab1be192f3762dbbea695eb760e0ce54629ff1
Mvlm-lp <mvinnylopes01@gmail.com>
2025-08-02 11:53:46 -0300

    Update RecebimentoService.java

--- projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/services/RecebimentoService.java
+++ projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/services/RecebimentoService.java
@@ -1,15 +1,15 @@
 package com.exemplo.ufba.tomorrow.gerenciador.service;
 
 import com.exemplo.ufba.tomorrow.gerenciador.dto.*;
+import com.exemplo.ufba.tomorrow.gerenciador.mapper.RecebimentoMapper;
 import com.exemplo.ufba.tomorrow.gerenciador.model.*;
-import com.exemplo.ufba.tomorrow.gerenciador.repository.*;
+import com.exemplo.ufba.tomorrow.gerenciador.repository.RecebimentoRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,20 +18,16 @@ public class RecebimentoService {
     private final RecebimentoRepository recebimentoRepository;
     private final EmpresaService empresaService;
 
-    public Recebimento salvar(Long empresaId, RecebimentoDTO dto) {
+    public RecebimentoResponseDTO salvar(Long empresaId, RecebimentoDTO dto) {
         Empresa empresa = empresaService.buscarPorId(empresaId);
-
-        Recebimento recebimento = new Recebimento();
-        recebimento.setCliente(dto.cliente());
-        recebimento.setValor(dto.valor());
-        recebimento.setVencimento(dto.vencimento());
-        recebimento.setRecebido(dto.recebido());
-        recebimento.setEmpresa(empresa);
-
-        return recebimentoRepository.save(recebimento);
+        Recebimento entity = RecebimentoMapper.toEntity(dto, empresa);
+        return RecebimentoMapper.toDTO(recebimentoRepository.save(entity));
     }
 
-    public List<Recebimento> listar(Long empresaId) {
-        return recebimentoRepository.findByEmpresaId(empresaId);
+    public List<RecebimentoResponseDTO> listar(Long empresaId) {
+        return recebimentoRepository.findByEmpresaId(empresaId)
+                .stream()
+                .map(RecebimentoMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
