1823e6da2a96ef295de258292f7a9fd2f7fa34ae
Mvlm-lp <mvinnylopes01@gmail.com>
2025-08-02 14:35:50 -0300

    Update EmpresaUserDetailsService.java

--- projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/config/EmpresaUserDetailsService.java
+++ projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/config/EmpresaUserDetailsService.java
@@ -1,1 +1,21 @@
 
+package br.ufba.tomorrow.gerenciador.security;
+
+
+import br.ufba.tomorrow.gerenciador.model.Empresa;
+import br.ufba.tomorrow.gerenciador.repository.EmpresaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+
+@RequiredArgsConstructor
+public class EmpresaUserDetailsService implements UserDetailsService {
+
+    private final EmpresaRepository empresaRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Empresa empresa = empresaRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Empresa não encontrada"));
+        return new EmpresaUserDetails(empresa);
+    }
+}
