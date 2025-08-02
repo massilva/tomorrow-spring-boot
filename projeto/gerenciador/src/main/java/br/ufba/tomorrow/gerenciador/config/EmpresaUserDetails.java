b5893d78184ce8fd04754b7fa9e0ec93019f91fa
Mvlm-lp <mvinnylopes01@gmail.com>
2025-08-02 14:38:03 -0300

    Update EmpresaUserDetails.java

--- projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/config/EmpresaUserDetails.java
+++ projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/config/EmpresaUserDetails.java
@@ -16,3 +16,32 @@ public class EmpresaUserDetails implements UserDetails {
     public EmpresaUserDetails(Empresa empresa) {
         this.empresa = empresa;
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "ROLE_EMPRESA");
+    }
+
+    @Override
+    public String getPassword() {
+        return empresa.getSenha();
+    }
+
+    @Override
+    public String getUsername() {
+        return empresa.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+}    
+    
