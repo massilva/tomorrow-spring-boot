565f3b205a045de6838c98eec7e07fc4b6079e1d
Mvlm-lp <mvinnylopes01@gmail.com>
2025-08-02 14:28:46 -0300

    Update EmpresaUserDetails.java apenas para criar o config

--- projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/config/EmpresaUserDetails.java
+++ projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/config/EmpresaUserDetails.java
@@ -1,1 +1,18 @@
+package br.ufba.tomorrow.gerenciador.security;
 
+import br.ufba.tomorrow.gerenciador.model.Empresa;
+import lombok.Getter;
+import org.springframework.security.core.*;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class EmpresaUserDetails implements UserDetails {
+
+    private final Empresa empresa;
+
+    public EmpresaUserDetails(Empresa empresa) {
+        this.empresa = empresa;
+    }
