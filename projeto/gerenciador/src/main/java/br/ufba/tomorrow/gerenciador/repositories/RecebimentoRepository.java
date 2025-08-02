da3d815ddcbae54352597df6006813f80c13e855
Mvlm-lp <mvinnylopes01@gmail.com>
2025-08-02 11:26:34 -0300

    Update RecebimentoRepository.java

--- projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/repositories/RecebimentoRepository.java
+++ projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/repositories/RecebimentoRepository.java
@@ -1,1 +1,11 @@
+package br.ufba.tomorrow.gerenciador.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.exemplo.br.ufba.tomorrow.gerenciador.model.*;
+
+import java.util.Optional;
+
+public interface RecebimentoRepository extends JpaRepository<Recebimento, Long> {
+    List<Recebimento> findByEmpresaId(Long empresaId);
+}
