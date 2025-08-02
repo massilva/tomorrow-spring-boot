70d5cab5fab8643b13f8006771fc7451627d8712
Mvlm-lp <mvinnylopes01@gmail.com>
2025-08-02 11:52:43 -0300

    Update RecebimentoResponseDTO.java

--- projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/dtos/RecebimentoResponseDTO.java
+++ projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/dtos/RecebimentoResponseDTO.java
@@ -1,1 +1,12 @@
+package com.exemplo.ufba.tomorrow.gerenciador.dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record RecebimentoResponseDTO(
+    Long id,
+    String cliente,
+    BigDecimal valor,
+    LocalDate vencimento,
+    boolean recebido
+) {}
