daed541d4f29508912ea018485e56ab1b2a41851
Mvlm-lp <mvinnylopes01@gmail.com>
2025-08-02 11:53:24 -0300

    Update RecebimentoMapper.java

--- projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/mappers/RecebimentoMapper.java
+++ projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/mappers/RecebimentoMapper.java
@@ -1,1 +1,28 @@
+package com.exemplo.ufba.tomorrow.gerenciador.mapper;
+
+import com.exemplo.ufba.tomorrow.gerenciador.dto.*;
+import com.exemplo.ufba.tomorrow.gerenciador.model.*;
+
+public class RecebimentoMapper {
+
+    public static Recebimento toEntity(RecebimentoDTO dto, Empresa empresa) {
+        Recebimento recebimento = new Recebimento();
+        recebimento.setCliente(dto.cliente());
+        recebimento.setValor(dto.valor());
+        recebimento.setVencimento(dto.vencimento());
+        recebimento.setRecebido(dto.recebido());
+        recebimento.setEmpresa(empresa);
+        return recebimento;
+    }
+
+    public static RecebimentoResponseDTO toDTO(Recebimento entity) {
+        return new RecebimentoResponseDTO(
+            entity.getId(),
+            entity.getCliente(),
+            entity.getValor(),
+            entity.getVencimento(),
+            entity.isRecebido()
+        );
+    }
+}
 
