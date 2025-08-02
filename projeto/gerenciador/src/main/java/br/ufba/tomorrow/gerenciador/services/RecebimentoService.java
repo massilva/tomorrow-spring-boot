b71305146361ebfe07cc126c3f8b547888a0f3ef
Mvlm-lp <mvinnylopes01@gmail.com>
2025-08-02 11:31:09 -0300

    Update RecebimentoService.java

--- projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/services/RecebimentoService.java
+++ projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/services/RecebimentoService.java
@@ -1,1 +1,37 @@
+package com.exemplo.ufba.tomorrow.gerenciador.service;
 
+import com.exemplo.ufba.tomorrow.gerenciador.dto.*;
+import com.exemplo.ufba.tomorrow.gerenciador.model.*;
+import com.exemplo.ufba.tomorrow.gerenciador.repository.*;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class RecebimentoService {
+
+    private final RecebimentoRepository recebimentoRepository;
+    private final EmpresaService empresaService;
+
+    public Recebimento salvar(Long empresaId, RecebimentoDTO dto) {
+        Empresa empresa = empresaService.buscarPorId(empresaId);
+
+        Recebimento recebimento = new Recebimento();
+        recebimento.setCliente(dto.cliente());
+        recebimento.setValor(dto.valor());
+        recebimento.setVencimento(dto.vencimento());
+        recebimento.setRecebido(dto.recebido());
+        recebimento.setEmpresa(empresa);
+
+        return recebimentoRepository.save(recebimento);
+    }
+
+    public List<Recebimento> listar(Long empresaId) {
+        return recebimentoRepository.findByEmpresaId(empresaId);
+    }
+}
