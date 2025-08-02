4601df602542353ddb99dc57a6b89f1e01e006a9
Mvlm-lp <mvinnylopes01@gmail.com>
2025-08-02 11:54:29 -0300

    Update RecebimentoRepository.java

--- projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/repositories/RecebimentoRepository.java
+++ projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/repositories/RecebimentoRepository.java
@@ -1,10 +1,8 @@
-package br.ufba.tomorrow.gerenciador.repositories;
+package com.exemplo.ufba.tomorrow.gerenciador.repository;
 
+import com.exemplo.ufba.tomorrow.gerenciador.model.Recebimento;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.exemplo.br.ufba.tomorrow.gerenciador.model.*;
-
-import java.util.Optional;
+import java.util.List;
 
 public interface RecebimentoRepository extends JpaRepository<Recebimento, Long> {
     List<Recebimento> findByEmpresaId(Long empresaId);
