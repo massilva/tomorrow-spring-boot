fef8ff8c0f9c5c4eb0fbaac648826406a2236f17
Mvlm-lp <mvinnylopes01@gmail.com>
2025-08-02 11:34:31 -0300

    Update Recebimento.java

--- projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/models/Recebimento.java
+++ projeto/gerenciador/src/main/java/br/ufba/tomorrow/gerenciador/models/Recebimento.java
@@ -1,1 +1,26 @@
+package com.exemplo.br.ufba.tomorrow.gerenciador.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Recebimento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cliente;
+    private BigDecimal valor;
+    private LocalDate vencimento;
+    private boolean recebido;
+
+    @ManyToOne
+    private Empresa empresa;
+}
